d39c452fdd557c1e9bee44d51123fb2d7b7e76bf
John Carlee <jcarlee@gmail.com>
2018-10-21 23:12:43 -0500

    first working file

---
+++ main.py
@@ -0,0 +1,18 @@
+# Parse inventory export
+#  Open file
+#  Use list of augs to search, return true or false
+# Compare aug_list to parse
+# Return all augs not found in parse
+
+augs = "Kerafyrm's Final Word", "Eye of the Sleeper", "Xegony's Final Word", "Cazic's Command", "Cazic's Order", \
+       "Cazic's Wish", "Cazic's Word", "Stone of Dragons", "Stone of Drakes", "Stone of Racnars",\
+       "Stone of Elder Magic", "Stone of Elder Endurance", "Stone of Elder Health", "Aviak Talon", "Aviak Feather", \
+       "Aviak Eye", "Kodiak Eye", "Kodiak Fur Hair", "Kodiak Claw", "Shark Eye", "Bloodthirsty Shark Skin", \
+       "Shark Tooth"
+
+filename = "C:\\Users\\jcarl\\AppData\\Local\\VirtualStore\\Program Files (x86)\\Everquest\\Apoth_inv.txt"
+
+file = open(filename, "r")
+
+if "Eye of the Sleeper" in file.read():
+    print("true")
