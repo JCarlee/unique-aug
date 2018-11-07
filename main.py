64a5d74511eb6cc3f4120fb2565fad73fa8fa9c1
jcarlee <jcarlee@gmail.com>
2018-11-07 17:17:27 -0600

    moved aug list creation from hard code to spreadsheet fetch

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ char_row_end = info_sheet.acell('D2').value
 
 # CHANGE THIS based on your directory storing exported files
 local_dir = "C:\\Users\\jcarl\\AppData\\Local\\VirtualStore\\Program Files (x86)\\Everquest\\"
-augs = []
-
-aug_cells = aug_sheet.range(aug_start_col + '1:' + aug_end_col + '1')  # String holding range of aug values
-for cell_val in aug_cells:                                             # Create list of augs using .value
-    augs.append(cell_val.value)
 
 # Sets inv_files to be a list of filename.txt
 inv_files = [os.path.basename(x) for x in glob.glob(local_dir + "*_inv.txt")]
+
+augs = []
+aug_cells = aug_sheet.range(aug_start_col + '1:' + aug_end_col + '1')  # String holding range of aug values
+for cell_val in aug_cells:                                             # Create list of augs using .value
+    augs.append(cell_val.value)
 
 for inv_file, column in zip(inv_files, range(int(char_row_start), (int(char_row_end)+1))):
     file = open(local_dir + inv_file, "r")                             # Open inv file, read
