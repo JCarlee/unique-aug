c1008eecdb3b1bb56c525c92fa39fae55d508e20
John Carlee <jcarlee@gmail.com>
2018-11-07 16:41:29 -0600

    Removed all hard coding - every value comes from spreadsheet

--- main.py
+++ main.py
@@ -14,24 +14,20 @@ aug_end_col = info_sheet.acell('B2').value
 char_row_start = info_sheet.acell('C2').value
 char_row_end = info_sheet.acell('D2').value
 
-# CHANGE based on your directory storing exported files
+# CHANGE THIS based on your directory storing exported files
 local_dir = "C:\\Users\\jcarl\\AppData\\Local\\VirtualStore\\Program Files (x86)\\Everquest\\"
+augs = []
 
-augs = aug_sheet.range(aug_start_col + '1:' + aug_end_col + '1')
-
-# augs = ["Kerafyrm's Final Word", "Eye of the Sleeper", "Xegony's Final Word", "Cazic's Command", "Cazic's Order",
-#         "Cazic's Wish", "Cazic's Word", "Stone of Dragons", "Stone of Drakes", "Stone of Racnars",
-#         "Stone of Elder Magic", "Stone of Elder Endurance", "Stone of Elder Health", "Aviak Talon", "Aviak Feather",
-#         "Aviak Eye", "Kodiak Eye", "Kodiak Fur Hair", "Kodiak Claw", "Shark Eye", "Bloodthirsty Shark Skin",
-#         "Shark Tooth"]
+aug_cells = aug_sheet.range(aug_start_col + '1:' + aug_end_col + '1')  # String holding range of aug values
+for cell_val in aug_cells:                                             # Create list of augs using .value
+    augs.append(cell_val.value)
 
 # Sets inv_files to be a list of filename.txt
 inv_files = [os.path.basename(x) for x in glob.glob(local_dir + "*_inv.txt")]
 
-
-for inv_file, column in zip(inv_files, range(char_row_start, (char_row_end+1))):
-    file = open(local_dir + inv_file, "r")  # Open inv file, read
-    file_string = file.read()  # Save inv file contents to a temporary string
+for inv_file, column in zip(inv_files, range(int(char_row_start), (int(char_row_end)+1))):
+    file = open(local_dir + inv_file, "r")                             # Open inv file, read
+    file_string = file.read()                                          # Save inv file contents to a temporary string
     cell_list = aug_sheet.range(aug_start_col + str(column) + ':' + aug_end_col + str(column))
     for aug, cell in zip(augs, cell_list):
         if aug not in file_string:
